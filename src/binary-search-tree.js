2e687a4beca2e08b8db4bc2b9908c872b51b6b0e
ketrololz <bqnexxx@gmail.com>
2025-01-05 17:18:44 +0300

    binary search tree task: create root, add and has methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,19 +8,53 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.treeRoot = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.treeRoot = addNode(this.treeRoot, data);
+
+    function addNode(node, data) {
+      const newNode = new Node(data);
+
+      if (!node) {
+        return newNode;
+      }
+
+      if (data === node.data) {
+        return node;
+      }
+
+      if (data > node.data) {
+        node.right = addNode(node.right, data);
+      } else {
+        node.left = addNode(node.left, data);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) { 
+    return hasData(this.treeRoot, data)
+    
+    function hasData(node, data) {
+
+      if (node.data === data) {
+        return true;
+      }
+      
+      if (data > node.data) {
+        return node.right ? hasData(node.right, data) : false;
+      } else {
+        return node.left ? hasData(node.left, data) : false;
+      }
+    }
   }
 
   find(/* data */) {
